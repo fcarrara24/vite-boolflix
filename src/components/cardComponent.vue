e6ba07f1a463f5ccf7d65f920111c2375320684c
unknown <f.carrara24@studenti.unibg.it>
2023-11-15 18:40:32 +0100

    working on basic hover effects

--- src/components/cardComponent.vue
+++ src/components/cardComponent.vue
@@ -1,19 +1,25 @@
 <template>
-    <div class="my-card d-flex flex-column p-3 ">
-        <div class="big-img-container w-100 ">
+    <div class="my-card d-flex flex-column p-3 w-100 text-white" :style="'background-image: url(' + builtImgUrl + ');'"
+        @mouseover="trigger()" @mouseleave="antiTrigger()">
+        <!-- <div class="big-img-container w-100 ">
             <img :src="builtImgUrl" alt="">
+        </div> -->
+        <div class="card-height">
+            <img :src="builtImgUrl" alt="" v-if="hovered">
         </div>
-        <div class="title">{{ title }}</div>
-        <div class="description">{{ original_title }}</div>
-        <div class="img-container">
-            <img :src="getFlagUrl" :alt="original_language">
-        </div>
-        <div class="description d-flex flex-row justify-content-start gap-2 align-items-center">
-            <!-- score -->
-            {{ this.getStar }}
-            <!-- stars -->
-            <div class="img-container" v-for="num in getStar">
-                <img src="../assets/images/star.png" alt="">
+        <div v-if="hovered">
+            <div class="title">{{ title }}</div>
+            <div class="description">{{ original_title }}</div>
+            <div class="img-container">
+                <img :src="getFlagUrl" :alt="original_language">
+            </div>
+            <div class="description d-flex flex-row justify-content-start gap-2 align-items-center">
+                <!-- score -->
+                {{ this.getStar }}
+                <!-- stars -->
+                <div class="img-container" v-for="num in getStar">
+                    <img src="../assets/images/star.png" alt="">
+                </div>
             </div>
         </div>
 
@@ -21,6 +27,8 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: 'cardComponent',
     props: {
@@ -36,10 +44,17 @@ export default {
     data() {
 
 
-        return {}
+        return {
+            hovered: false
+        }
     },
     methods: {
-
+        trigger() {
+            this.hovered = true
+        },
+        antiTrigger() {
+            this.hovered = false
+        }
     },
     computed: {
         getFlagUrl() {
@@ -47,10 +62,14 @@ export default {
             if (parsedString === "EN") {
                 parsedString = "US"
             } else if (parsedString === "JA") {
-                parsedString = "JS"
+                parsedString = "JP"
+
+            } else if (parsedString === "ZH") {
+                parsedString = "SG"
 
             }
             console.log(parsedString)
+
             return 'https://flagsapi.com/' + parsedString + '/flat/64.png';
         },
         builtImgUrl() {
@@ -66,11 +85,27 @@ export default {
 <style lang="scss" scoped>
 .my-card {
     color: black;
-    background-color: white;
-    min-height: 200px;
-    min-width: 200px;
+    background-color: black;
+    min-height: 20vh;
+    min-width: 100%;
     height: 100%;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
 
+
+}
+
+.my-card:hover {
+    //transform: rotateY(180deg);
+    background-image: none !important;
+    ;
+}
+
+.card-height {
+    //this will act as an image proportion keeper for the display of elements
+    height: 500px;
+    width: 400px;
 }
 
 .big-img-container {
